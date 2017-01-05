74f52d22b1412bcd18d471b2e371b7b779ed1292
Robbie Brandrick <Robbie Brandrick>
2017-01-05 07:05:51 -0500

    Basic ASP Core MVC Configuration Setup

--- src/Spellthis/Startup.cs
+++ src/Spellthis/Startup.cs
@@ -16,6 +16,9 @@ namespace Spellthis
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +31,16 @@ namespace Spellthis
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=SpellThis}/{action=Index}/{id?}"
+                );
             });
+
         }
     }
 }
