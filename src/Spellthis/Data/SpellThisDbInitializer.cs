b4f3813ba313ed4f44e3d9962b71116c0279ebab
Robbie Brandrick <me@robbiebrandrick.com>
2017-03-19 07:52:57 -0400

    Added user and role seed data

--- src/Spellthis/Data/SpellThisDbInitializer.cs
+++ src/Spellthis/Data/SpellThisDbInitializer.cs
@@ -1,4 +1,7 @@
-﻿using Spellthis.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Spellthis.Models;
+using Spellthis.Models.Account;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +17,60 @@ namespace Spellthis.Data
 
             context.Database.EnsureCreated();
 
-            if (context.Words.Any())
+            if (!context.Words.Any())
             {
-                return;
+                SeedWords(context);
             }
-            
+
+            if (!context.UserRoles.Any())
+            {
+                SeedRoles(context);
+            }
+
+            if (!context.Users.Any())
+            {
+                SeedUsers(context);
+            }
+
+        }
+
+        private static void SeedUsers(SpellThisContext context)
+        {
+            var admin = new ApplicationUser
+            {
+                UserName = "admin@spellthis.com",
+                NormalizedUserName = "admin@spellthis.com",
+                Email = "admin@spellthis.com",
+                NormalizedEmail = "admin@spellthis.com",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var password = new PasswordHasher<ApplicationUser>();
+            var hashed = password.HashPassword(admin, "password");
+            admin.PasswordHash = hashed;
+            var userStore = new UserStore<ApplicationUser>(context);
+            userStore.CreateAsync(admin).Wait();
+            userStore.AddToRoleAsync(admin, "admin").Wait();
+        }
+
+        private static void SeedRoles(SpellThisContext context)
+        {
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+
+            if (!context.Roles.Any(r => r.Name == "admin"))
+            {
+
+                roleStore.CreateAsync(new IdentityRole { Name = "admin", NormalizedName = "admin" }).Wait();
+
+            }
+
+        }
+
+        private static void SeedWords(SpellThisContext context)
+        {
             var words = new List<Word>()
             {
                 new Word
@@ -50,9 +102,6 @@ namespace Spellthis.Data
             }
 
             context.SaveChanges();
-
-
         }
-
     }
 }
